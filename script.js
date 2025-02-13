7a504cb15f6e12a3f52514a4412402b8cd71d4f7
승얄 <164878414+seungYal@users.noreply.github.com>
2025-02-13 23:42:20 +0900

    이미지 변경 및 세 번째 이미지 스크롤 수정

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let currentIndex = 0;
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".slide, .tall-slide");
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
         slide.classList.remove("active");
+        slide.style.display = "none"; // 모든 이미지 숨김
+
         if (i === index) {
             slide.classList.add("active");
+            slide.style.display = "block"; // 선택한 이미지만 표시
         }
     });
 }
